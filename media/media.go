8a63fc6f22f214f5dc8451752cb6f46ed4e44f56
zojl <2863156+zojl@users.noreply.github.com>
2024-02-18 04:19:56 +0100

    Fix only first image display while proxying

--- media/media.go
+++ media/media.go
@@ -6,7 +6,7 @@ import (
 )
 
 func GetPostPath(path string) (string) {
-	suffix := "?embed=1&mode=tme"
+	suffix := "?embed=1&mode=tme&single=1"
 	path = strings.TrimPrefix(path, "media/")
 	lastDotIndex := strings.LastIndex(path, ".")
 	if lastDotIndex == -1 {
