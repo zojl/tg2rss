f6b8bea82a174fb5b94aa6a285c15e2a51e26ec4
zojl <2863156+zojl@users.noreply.github.com>
2024-02-18 05:24:17 +0100

    Added video previews proxying, but blur can be broken

--- parser/parser.go
+++ parser/parser.go
@@ -51,11 +51,19 @@ func ParseMedia(html string) (string, error) {
 	}
 
 	videos := post.Find("a.tgme_widget_message_video_player")
-	if videos.Length() > 0 {
+	if videos.Length() > 0 && !videos.First().HasClass("not_supported") {
 		video := videos.First()
 		videoWrap := video.Find(".tgme_widget_message_video_wrap").First()
 		videoLink, _ := videoWrap.Find("video").First().Attr("src")
 		return videoLink, nil
+	}
+
+	videoPreviews := post.Find(".tgme_widget_message_video_thumb")
+	if videoPreviews.Length() > 0 {
+		videoPreview := videoPreviews.First()
+		styleRaw, _ := videoPreview.Attr("style")
+		fmt.Println(styleRaw)
+		return getBackgroundImage(styleRaw), nil
 	}
 
 	return "", errors.New("post not found or has no media")
@@ -80,76 +88,104 @@ func getItem(post *goquery.Selection) rss.Item {
 	
 	if hasMedia {
 		post.Find("a.tgme_widget_message_photo_wrap").Each(func(j int, photoContent *goquery.Selection) {
-			styleRaw, _ := photoContent.Attr("style")
-			linkHref, _ := photoContent.Attr("href")
-			item.Content = item.Content + fmt.Sprintf(
-				"<a href='%s'><img src='%s' alt='post image'></a><br>",
-				linkHref,
-				getPhotoUrl(linkHref, styleRaw),
-			)
+			item.Content = item.Content + makePhotoBlock(photoContent)
 		})
 		
 		post.Find("a.tgme_widget_message_video_player").Each(func(j int, previewContent *goquery.Selection) {
 			if (previewContent.HasClass("not_supported")) {
-				styleRaw, _ := previewContent.Find("i.tgme_widget_message_video_thumb").First().Attr("style")
-				linkHref, _ := previewContent.Attr("href")
-				item.Content = item.Content + fmt.Sprintf(
-					"<a href='%s'><img src='%s' alt='video preview'></a><br>",
-					linkHref,
-					getBackgroundImage(styleRaw),
-				)
+				item.Content = item.Content + makeUnsupportedVideoPreview(previewContent)
 				return
 			}
 
-			videoWrap := previewContent.Find(".tgme_widget_message_video_wrap").First()
-			videoWrapStyle, _ := videoWrap.Attr("style")
-			videoLink := ""
-			if os.Getenv("PROXY_MEDIA") == "true" {
-				linkHref, _ := previewContent.Attr("href")
-				identifier, _ := getPostIdentifier(linkHref)
-				videoLink = fmt.Sprintf("%s/media%s.%s", os.Getenv("MEDIA_HOST"), identifier, "mp4")
-			} else {
-				videoLink, _ = videoWrap.Find("video").First().Attr("src")
-			}
-			videoPlayer := fmt.Sprintf("<div style=\"%s\"><video src=\"%s\" width=\"100%%\" height=\"100%%\"></video></div>", videoWrapStyle, videoLink)
+			videoPlayer := makeVideoPlayer(previewContent)
 			item.Content = item.Content + videoPlayer + "<br>"
 		})
 	}
-	
+
 	post.Find(".tgme_widget_message_text").Each(func(j int, text *goquery.Selection) {
 		if text.Find(".tgme_widget_message_text").Length() > 0 {
 			return
 		}
-		
+
 		textHtml, _ := text.Html()
 		item.Content = item.Content + textHtml
 		replaceLineBreaks(text)
 		item.Description = item.Description + text.Text()
 	})
-	
+
 	messageDate := post.Find(".tgme_widget_message_date").First()
 	item.Link, _ = messageDate.Attr("href")
-	
+
 	postTime, _ := messageDate.Find("time").First().Attr("datetime")
 	item.Created, _ = time.Parse(timeLayout, postTime)
-	
+
 	unsupported := post.Find(".message_media_not_supported_label")
 	if (unsupported.Length() != 0) && (len(item.Content) == 0) {
 		item.Content = fmt.Sprintf("Unsupported post, <a href='%s'>view in Telegram</a>", item.Link)
 		item.Description = "Unsopported post: " + item.Link
 	}
-	
+
 	title := item.Description
 	if hasMedia {
 		title = "🖼️" + title
 	}
 	if len([]rune(title)) > maxTitleLength {
-		item.Title = string([]rune(title)[:maxTitleLength - 3]) + "..."
+		item.Title = string([]rune(title)[:maxTitleLength-3]) + "..."
 	} else {
 		item.Title = title
 	}
 
 	return item
+}
+
+func makeUnsupportedVideoPreview(previewContent *goquery.Selection) string {
+	linkHref, _ := previewContent.Attr("href")
+	outImageSrc := ""
+	if os.Getenv("PROXY_MEDIA") == "true" {
+		identifier, _ := getPostIdentifier(linkHref)
+		outImageSrc = fmt.Sprintf("%s/media%s.jpg", os.Getenv("MEDIA_HOST"), identifier)
+	} else {
+		styleRaw, _ := previewContent.Find("i.tgme_widget_message_video_thumb").First().Attr("style")
+		outImageSrc = getBackgroundImage(styleRaw)
+	}
+	return fmt.Sprintf(
+		"<a href=\"%s\" style=\"filter: blur(15px)\"><img src=\"%s\" alt=\"video preview\"></a><br>",
+		linkHref,
+		outImageSrc,
+	)
+}
+
+func makePhotoBlock(photoContent *goquery.Selection) string {
+	styleRaw, _ := photoContent.Attr("style")
+	linkHref, _ := photoContent.Attr("href")
+	backgroundImageSrc := getBackgroundImage(styleRaw)
+	outImageSrc := backgroundImageSrc
+	if os.Getenv("PROXY_MEDIA") == "true" {
+		extension := extractExtension(backgroundImageSrc)
+		identifier, _ := getPostIdentifier(linkHref)
+		outImageSrc = fmt.Sprintf("%s/media%s.%s", os.Getenv("MEDIA_HOST"), identifier, extension)
+	}
+
+	return fmt.Sprintf(
+		"<a href='%s'><img src='%s' alt='post image'></a><br>",
+		linkHref,
+		outImageSrc,
+	)
+}
+
+func makeVideoPlayer(previewContent *goquery.Selection) string {
+	videoWrap := previewContent.Find(".tgme_widget_message_video_wrap").First()
+	videoWrapStyle, _ := videoWrap.Attr("style")
+	videoLink := ""
+	if os.Getenv("PROXY_MEDIA") == "true" {
+		linkHref, _ := previewContent.Attr("href")
+		identifier, _ := getPostIdentifier(linkHref)
+		videoLink = fmt.Sprintf("%s/media%s.%s", os.Getenv("MEDIA_HOST"), identifier, "mp4")
+	} else {
+		videoLink, _ = videoWrap.Find("video").First().Attr("src")
+	}
+	videoPlayer := fmt.Sprintf("<div style=\"%s\"><video src=\"%s\" width=\"100%%\" height=\"100%%\"></video></div>", videoWrapStyle, videoLink)
+	return videoPlayer
 }
 
 func getBackgroundImage(inline string) string {
@@ -172,17 +208,6 @@ func getPostIdentifier(postUrl string) (string, error) {
 	// Получение пути из разобранного URL
 	path := parsedURL.Path
 	return path, nil
-}
-
-func getPhotoUrl(link string, inline string) string {
-	backgroundImageSrc := getBackgroundImage(inline)
-	if os.Getenv("PROXY_MEDIA") == "true" {
-		extension := extractExtension(backgroundImageSrc)
-		identifier, _ := getPostIdentifier(link)
-		return fmt.Sprintf("%s/media%s.%s", os.Getenv("MEDIA_HOST"), identifier, extension)
-	}
-
-	return backgroundImageSrc
 }
 
 func extractExtension(url string) string {
